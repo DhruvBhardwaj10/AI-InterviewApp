36651546a255413653881b96702fdef0e2e3ca14
Dhruv Bhardwaj <dhruvbhardwaj12a14@gmail.com>
2024-08-26 18:25:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://ai-interview-backend-five.vercel.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
